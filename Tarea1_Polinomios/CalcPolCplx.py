b16580aa862275faf9e326f34161170539f361ef
alan <allanstone19@gmail.com>
2016-08-14 23:37:53 -0500

    Simplificar en vez de expandir en la operacion de division

--- Tarea1_Polinomios/CalcPolCplx.py
+++ Tarea1_Polinomios/CalcPolCplx.py
@@ -15,11 +15,11 @@ from sympy import *
 
 class CalcPol:
 	'''
-	Linea de documentacion de la clase CalcPol
+		Linea de documentacion de la clase CalcPol
 	'''
 	def __init__(self, variable, grado,*coeficientes):
 		'''
-		Constructor de CalcPol
+			Constructor de CalcPol
 		'''
 		self.variable=symbols(variable)
 		self.expresion=0
@@ -36,7 +36,6 @@ class CalcPol:
 	def valorPunto(self,punto):
 		return self.expresion.evalf(subs={self.variable:punto})
 
-
 	def sumaPol(self,pol):
 		return self.expresion+pol.expresion
 
@@ -47,7 +46,7 @@ class CalcPol:
 		return expand(self.expresion*pol.expresion)
 
 	def diviPol(self,pol):
-		return expand(self.expresion/pol.expresion)
+		return simplify(self.expresion/pol.expresion)
 
 	def derivaPol(self):
 		return diff(self.expresion,self.variable)
@@ -76,7 +75,6 @@ class CalcCplx():
 		'''
 			ang= atan(cplx.imag/cplx.real)
 			ang= polar(cplx)[1] en radianes
-			
 		'''
 		return atan2(self.cplx.imag,self.cplx.real)
 
@@ -93,44 +91,54 @@ class CalcCplx():
 		return self.cplx/cplx.cplx
 
 	def potCplx(self,grado):
-		return self.cplx*cplx.cplx
+		return self.cplx**grado
 
-
-		
 
 if __name__ == '__main__':
-	"""
-	grado1=int(input("Dame el grado del primer polinomio:"))
-	variable1=input("Dame el nombre de la variable independiente: ")[0]
-	grado2=int(input("Dame el grado del segundo polinomio:"))
-	variable2=input("Dame el nombre de la variable independiente: ")[0]
-	#instanciamos dos polinomios
-	pol1=CalcPol(variable1,grado1)
-	pol2=CalcPol(variable2,grado2)
-	#Evaluamos en un punto
-	punto=float(input("Dame el valor a evaluar para el primer polinomio: "))
-	print("Operaciones con Polinomios")
-	print("Valor en %s con %s=%.2f: %.2f" %(pol1.expresion,variable1,punto,pol1.valorPunto(punto)))
-	#Hacemos todas las operaciones posiles
-	print("Suma de %s con %s = %s "%(pol1.expresion,pol2.expresion,pol1.sumaPol(pol2)))
-	print("Resta de %s con %s = %s "%(pol1.expresion,pol2.expresion,pol1.restaPol(pol2)))
-	print("Multiplicacion de %s con %s = %s "%(pol1.expresion,pol2.expresion,pol1.multiPol(pol2)))
-	print("Division de %s con %s = %s "%(pol1.expresion,pol2.expresion,pol1.diviPol(pol2)))
-	#Integral y derivada
-	print("Derivada de %s respecto a %s = %s "%(pol1.expresion,pol1.variable,pol1.derivaPol()))
-	print("Integral de %s respecto a %s = %s "%(pol2.expresion,pol2.variable,pol2.integraPol()))
-	"""
-	#Operaciones con complejos
+	#Operaciones con Polinomios
+	try:
+		grado1=int(input("Dame el grado del primer polinomio:"))
+		variable1=input("Dame el nombre de la variable independiente: ")[0]
+		grado2=int(input("Dame el grado del segundo polinomio:"))
+		variable2=input("Dame el nombre de la variable independiente: ")[0]
+		#instanciamos dos polinomios
+		pol1=CalcPol(variable1,grado1)
+		pol2=CalcPol(variable2,grado2)
+		#Evaluamos en un punto
+		punto=float(input("Dame el valor a evaluar para el primer polinomio: "))
+		print("Operaciones con Polinomios")
+		print("Valor en %s con %s=%.2f: %.2f" %(pol1.expresion,variable1,punto,pol1.valorPunto(punto)))
+		#Hacemos todas las operaciones posiles
+		print("Suma de %s con %s = %s "%(pol1.expresion,pol2.expresion,pol1.sumaPol(pol2)))
+		print("Resta de %s con %s = %s "%(pol1.expresion,pol2.expresion,pol1.restaPol(pol2)))
+		print("Multiplicacion de %s con %s = %s "%(pol1.expresion,pol2.expresion,pol1.multiPol(pol2)))
+		print("Division de %s con %s = %s "%(pol1.expresion,pol2.expresion,pol1.diviPol(pol2)))
+		#Integral y derivada
+		print("Derivada de %s respecto a %s = %s "%(pol1.expresion,pol1.variable,pol1.derivaPol()))
+		print("Integral de %s respecto a %s = %s "%(pol2.expresion,pol2.variable,pol2.integraPol()))
 
-	cplx1=CalcCplx(1,2)
-	cplx2=CalcCplx(0,-1)
-	print("Operaciones con Complejos")
-	print("Suma de %s con %s = %s" %(cplx1,cplx2,cplx1.sumaCplx(cplx2)))
-	print("Resta de %s con %s = %s" %(cplx1,cplx2,cplx1.restaCplx(cplx2)))
-	print("Multiplicacion de %s con %s = %s" %(cplx1,cplx2,cplx1.multiCplx(cplx2)))
-	print("Division de %s con %s = %s" %(cplx1,cplx2,cplx1.diviCplx(cplx2)))
-	print("Módulo de %s = %s" %(cplx1,cplx1.modCplx()))
-	print("Ángulo de %s = %s" %(cplx2,cplx2.angCplx()))
+	#Operaciones con complejos
+		cplx1Real=float(input("Dame el valor de la parte real del primer complejo: "))
+		cplx1Imag=float(input("Dame el valor de la parte imaginaria del primer complejo: "))
+		cplx2Real=float(input("Dame el valor de la parte real del primer complejo: "))
+		cplx2Imag=float(input("Dame el valor de la parte imaginaria del primer complejo: "))
+
+		#instanciamos los complejos
+		cplx1=CalcCplx(cplx1Real,cplx1Imag)
+		cplx2=CalcCplx(cplx2Real,cplx2Imag)
+		grado=2
+		print("Operaciones con Complejos")
+		print("Suma de %s con %s = %s" %(cplx1,cplx2,cplx1.sumaCplx(cplx2)))
+		print("Resta de %s con %s = %s" %(cplx1,cplx2,cplx1.restaCplx(cplx2)))
+		print("Multiplicacion de %s con %s = %s" %(cplx1,cplx2,cplx1.multiCplx(cplx2)))
+		print("Division de %s con %s = %s" %(cplx1,cplx2,cplx1.diviCplx(cplx2)))
+		print("Potencia de %s^%d = %s" %(cplx1,grado,cplx1.diviCplx(cplx2)))
+		print("Módulo de %s = %s" %(cplx1,cplx1.modCplx()))
+		print("Ángulo de %s = %s" %(cplx2,cplx2.angCplx()))
+		
+	except ValueError as ve:
+		print("Ingrese solo números porfavor....\n",ve)
+	
 
 
 
