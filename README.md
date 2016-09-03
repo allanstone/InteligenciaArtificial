89c6673b1f9d96f0a99b7bef0267db25fb72b693
alan <allanstone19@gmail.com>
2016-09-02 20:54:02 -0500

    Agregando docstrings de la clase villano

--- README.md
+++ README.md
@@ -40,3 +40,18 @@ Repositorio donde se alojarán las tareas y proyectos de la materia de Inteligen
 
         ->Texto Cifrado: ROFSACSLIGIRNACTAEMISAPRAOTS
 
+## TAREA 3
+* Puede revisar la defición de la tarea en la siguiente [presentación](http://dicyg.fi-c.unam.mx:8080/lalo/ia/presentaciones/introduccion-a-la-inteligencia-artificial#page=19). 
+
+        Consiste en hacer un código que se encargue de crear al mejor villano mediante un algoritmo genético.
+            1. El programa creará la primera generación de 100 villanos con cada habilidad como un número
+            aleatorio entre 0 y 9.
+            2. El programa elegirá aleatoriamente a un villano por cada columna para tomar ese valor
+            como la habilidad de un nuevo villano (Megamente).
+            3. El programa repetirá el paso 2 para generar otro villano (Sedusa).
+            4. Al tener a los dos nuevos supervillanos, el programa deberá mezclar sus habilidades utilizando
+            un número aleatorio entre 1 y 10, como se muestra en el ejemplo:
+            5. Repetir los pasos 2, 3 y 4 hasta obtener 10 nuevos supervillanos (Sedumente[0..9]).
+            6. Finalmente, aplicar los pasos 2, 3 y 4 sobre la nueva población de 10 Sedumentes para
+            obtener al mejor supervillano.
+
--- Tarea 3/AlgoritmoGenetico/mejorVillano.py
+++ Tarea 3/AlgoritmoGenetico/mejorVillano.py
@@ -15,17 +15,27 @@ from random import randint
 class Villain:
     """
 	    Clase villano.
-	    Metodos:
-	       - encrypt: Cifra el mensaje pasado al constructor con la llave
+	    Atributos:
+	       - abilities: Una lista con los valores de las habilidades del villano
 
 	    Se puede usar de la siguiente manera:
-	        >>> 
+	        >>> darthVader=Villain()
+            >>> darthVader.abilities
+            [2,3,0,3,7,1,2,4,3,8]
+            #También se puede pasar una lista:
+            >>> kidBu=Villain([4,4,1,2,1,1,5,4,3,2])
+            >>> kidBu.abilities
+            [4,4,1,2,1,1,5,4,3,2]
     """
 
     def __init__(self,*abilities):
-        """
-            Constructor de Villain
-        """
+        '''
+           Constructor de la clase  puede instanciar de dos maneras diferentes:
+            -Sin parámetros, las habilidades se crearán aleatoriamente
+            -Se pasan  una lista con los valores para las habilidades
+            :param abilities: Lista con los valores de las habilidades.
+            :type abilities: list.
+        '''
         if abilities:
             self.abilities=abilities[0]
         else:
@@ -35,24 +45,56 @@ class Villain:
             self.abilities=abilities
 
 def populate(max):
+    """
+        Crea una población de villanos igual al valor que se le pasa como parámetro.
+
+        :param max: Número máximo de villanos que se requiere.
+        :type max: int.
+        :returns: list population-- Lista con los villanos y sus valores.
+    """
     population=[]
     for i in range(0,max):
         population.append(Villain())
     return population
 
 def createGeneration(population):
+    """
+        Crea una nueva generación con un villano con valores seleccionados aleatoriamente de la población.
+
+        :param population: Lista de villanos de la que se obtendrá el nuevo villano.
+        :type population: list.
+        :returns: Obj Villain-- Villano con valores seleccionados de la población.
+    """
     abilities=[]
     for i in range(0,10):
         abilities.append(population[randint(0,len(population)-1)].abilities[i])
     return Villain(abilities)
 
 def createCrossbreed(Villain1,Villain2):
+    """
+        Crea una nuevo villano de de la combinación de otros dos.
+
+        :param Villain1: Villano del que se obtendrá un número aleatorio de habilidades.
+        :type Villain1: Villain.
+        :param Villain2: Villano del que se obtendrá el resto de las habilidades.
+        :type Villain2: Villain.
+        :returns: Obj Villain-- Villano con valores seleccionados de los otros dos.
+    """
     abilities=[]
     split=randint(0,9)
     abilities=Villain1.abilities[:split]+Villain2.abilities[split:]
     return Villain(abilities)
 
 def createNewPopulation(Villain1,Villain2):
+    """
+        Crea una nueva generación de villanos creados apartir de otros dos.
+
+        :param Villain1: Villano del que se obtendrá un número aleatorio de habilidades.
+        :type Villain1: Villain.
+        :param Villain2: Villano del que se obtendrá el resto de las habilidades.
+        :type Villain2: Villain.
+        :returns: list-- Lista de nueva generación de villanos.
+    """
     newPopulation=[]
     for newVillain in range(0,10):
         newPopulation.append(createCrossbreed(megamente,sedusa))
